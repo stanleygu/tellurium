cd4819121345074f0b6fe163d6524b0f76181a58
Stanley Gu <stanleygu@gmail.com>
2014-11-11 23:12:10 -0800

    Added make_submodel

--- tellurium/analysis/match.py
+++ tellurium/analysis/match.py
@@ -1,5 +1,6 @@
 import _annotations
 
+
 def getMatchingSpecies(m1, m2, logging=False):
     '''
     Returns a list of species with matching annotations URIs
@@ -13,20 +14,29 @@ def getMatchingSpecies(m1, m2, logging=False):
         for s2 in m2.species:
             match = _annotations.matchSpeciesChebi(s1, s2, logging=logging)
             if match:
-                if len(match['exact']) or len(match['children']) or len(match['parents']):
+                if len(match['exact']) or \
+                        len(match['children']) or \
+                        len(match['parents']):
                     matches.append(match)
     return matches
+
 
 def printMatchingSpecies(matches):
     '''Prints the matches from getMatchingSpecies
     '''
     for match in matches:
         if len(match['exact']):
-            print '%s exactly matches %s' % (match['exact'][0]['id'], match['id'])
+            print '%s exactly matches %s' % (
+                match['exact'][0]['id'], match['id'])
         if len(match['parents']):
-            print '%s %s %s' % (match['parents'][0]['id'], match['parents'][0]['data']['type'], match['id'])
+            print '%s %s %s' % (
+                match['parents'][0]['id'],
+                match['parents'][0]['data']['type'], match['id'])
         if len(match['children']):
-            print '%s %s %s' % (match['children'][0]['id'], match['children'][0]['data']['type'], match['id'])
+            print '%s %s %s' % (
+                match['children'][0]['id'],
+                match['children'][0]['data']['type'], match['id'])
+
 
 def getMatchingReactions(modelOrList, idToMatch):
     '''
@@ -50,3 +60,108 @@ def getMatchingReactions(modelOrList, idToMatch):
                 matches.append(r)
     return matches
 
+
+def getAllReactionParameters(r):
+    '''
+    Find all reactions
+    '''
+    # list to hold found parameters
+    params = []
+    # initialize frontier to search
+    frontier = []
+    for i in range(r.kinetic_law.math.getNumChildren()):
+        frontier.append(r.kinetic_law.math.getChild(i))
+    while len(frontier) > 0:
+        node = frontier.pop()
+
+        for i in range(node.getNumChildren()):
+            frontier.append(node.getChild(i))
+
+        if node.isName() and r.model.getParameter(node.getName()):
+            params.append(r.model.getParameter(node.getName()))
+
+    return params
+
+
+def getAllReactionFunctions(r):
+    '''Find all functions used in a reaction kinetic law
+    '''
+    # list to hold found functions
+    functions = []
+    # initialize frontier to search
+    frontier = []
+    for i in range(r.kinetic_law.math.getNumChildren()):
+        frontier.append(r.kinetic_law.math.getChild(i))
+    while len(frontier) > 0:
+        node = frontier.pop()
+        for i in range(node.getNumChildren()):
+            frontier.append(node.getChild(i))
+
+        if node.isFunction() and r.model.getFunctionDefinition(node.getName()):
+            functions.append(r.model.getFunctionDefinition(node.getName()))
+
+    return functions
+
+
+def make_submodel(r):
+    '''Create a submodel from a reaction
+    '''
+    import libsbml
+    doc = r.getSBMLDocument()
+    newDoc = libsbml.SBMLDocument(doc.getLevel(), doc.getVersion())
+    newMod = newDoc.createModel()
+    newMod.setId(r.model.getId() + '_' + r.getId())
+
+    # Holding elements that will need to be cloned
+    species = set()
+    compartments = set()
+    parameters = set()
+    functions = set()
+    units = set()
+
+    for reactant in r.reactants:
+        s = r.model.getSpecies(reactant.getSpecies())
+        species.add(s)
+        compartments.add(r.model.getCompartment(s.getCompartment()))
+    for product in r.products:
+        s = r.model.getSpecies(product.getSpecies())
+        species.add(s)
+        compartments.add(r.model.getCompartment(s.getCompartment()))
+    for modifier in r.modifiers:
+        s = r.model.getSpecies(modifier.getSpecies())
+        species.add(s)
+        compartments.add(r.model.getCompartment(s.getCompartment()))
+    for p in getAllReactionParameters(r):
+        parameters.add(p)
+        if p.units:
+            unit_def = r.model.getUnitDefinition(p.units)
+            if isinstance(unit_def, libsbml.UnitDefinition):
+                units.add(unit_def)
+    for function in getAllReactionFunctions(r):
+        functions.add(function)
+    for p in r.kinetic_law.parameters:
+        if p.units:
+            unit_def = r.model.getUnitDefinition(p.units)
+            if isinstance(unit_def, libsbml.UnitDefinition):
+                units.add(unit_def)
+    newMod.addReaction(r.clone())
+
+    # Clone additional elements
+    for s in species:
+        newMod.addSpecies(s.clone())
+
+    for p in parameters:
+        newMod.addParameter(p.clone())
+    while len(compartments):
+        c = compartments.pop()
+        newMod.addCompartment(c.clone())
+        if c.getOutside():
+            compartments.add(
+                r.model.getCompartment(c.getOutside())
+            )
+    for u in units:
+        newMod.addUnitDefinition(u.clone())
+    for f in functions:
+        newMod.addFunctionDefinition(f.clone())
+
+    return newDoc
