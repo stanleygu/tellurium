93a627347c3c0c71b1c3c2ced02933d27720dc19
Stanley Gu <stanleygu@gmail.com>
2016-01-10 12:07:57 -0800

    Diff diagram working

--- tellurium/visualization/DiffDiagram.py
+++ tellurium/visualization/DiffDiagram.py
@@ -16,8 +16,8 @@ class DiffDiagram():
 
         # List of the two sbml documents to create diff
         self.doc = []
-        # List of two sets, containing the IDs of overlapping species
-        self.matches = [set(), set()]
+        # Dict of species id (from model 1) to description of match in model 2
+        self.matches = [dict(), dict()]
         # The pygraphviz graph object
         self.G = pgv.AGraph(strict=False, directed=True)
 
@@ -36,22 +36,22 @@ class DiffDiagram():
 
         # Create set of overlapping species IDs
         for entry in matching_species:
-            self.matches[0].add(entry['id'])
-            for sub_entry in entry['children']:
-                self.matches[1].add(sub_entry['id'])
-            for sub_entry in entry['parents']:
-                self.matches[1].add(sub_entry['id'])
+            self.matches[0][entry['id']] = entry
+            for sub_entry in entry['children'] + entry['parents'] + entry['exact']:
+                self.matches[1][sub_entry['id']] = entry
 
         first_color = 'blue'
         second_color = 'red'
         match_color = 'green'
+
+        # Creating graph for first model
         for s in self.doc[0].model.species:
-            if s.getId() in self.matches[0]:
-                label = s.getName() + ' ()'
-            elif s.getName():
+            if s.getName():
                 label = s.getName()
             else:
                 label = s.getId()
+            if s.getId() in self.matches[0]:
+                label += " ({})".format(self._create_matched_description(s.getId()))
 
             if s.getId() in self.matches[0]:
                 color = match_color
@@ -71,8 +71,52 @@ class DiffDiagram():
             for s in r.modifiers:
                 self.G.add_edge(s.getSpecies(), r.getId(), arrowhead='odot', color=first_color)
 
+        # Creating graph for second model
+        for s in self.doc[1].model.species:
+            # Skip creating species node for matches
+            if s.getId() in self.matches[1]:
+                continue
+            if s.getName():
+                label = s.getName() + ' ({})'.format(s.getId())
+            else:
+                label = s.getId()
+            self.G.add_node(s.getId(), label=label, color=second_color)
+        for r in self.doc[1].model.reactions:
+            if r.getName():
+                label = r.getName()
+            else:
+                label = r.getId()
+            self.G.add_node(r.getId(), label=label, shape='box', color=second_color)
+            for s in r.reactants:
+                if s.getSpecies() in self.matches[1]:
+                    matched_id = self.matches[1][s.getSpecies()]['id']
+                    self.G.add_edge(matched_id, r.getId(), arrowhead='none', color=second_color)
+                else:
+                    self.G.add_edge(s.getSpecies(), r.getId(), arrowhead='none', color=second_color)
+            for s in r.products:
+                if s.getSpecies() in self.matches[1]:
+                    matched_id = self.matches[1][s.getSpecies()]['id']
+                    self.G.add_edge(r.getId(), matched_id, color=second_color)
+                else:
+                    self.G.add_edge(r.getId(), s.getSpecies(), color=second_color)
+            for s in r.modifiers:
+                if s.getSpecies() in self.matches[1]:
+                    matched_id = self.matches[1][s.getSpecies()]['id']
+                    self.G.add_edge(matched_id, r.getId(), arrowhead='odot', color=second_color)
+                else:
+                    self.G.add_edge(s.getSpecies(), r.getId(), arrowhead='odot', color=second_color)
 
-
+    def _create_matched_description(self, id):
+        entry = self.matches[0][id]
+        if entry['children']:
+            matched_id = entry['children'][0]['id']
+            return '{} {} {}'.format(matched_id, entry['children'][0]['data']['type'], id)
+        elif entry['parents']:
+            matched_id = entry['parents'][0]['id']
+            return '{} {} {}'.format(id, entry['parents'][0]['data']['type'], matched_id)
+        elif entry['exact']:
+            matched_id = entry['exact'][0]['id']
+            return '{} {} {}'.format(id, 'exactly matches', matched_id)
 
         # for i, s in enumerate(self.model.species):
         #     if s.getName():
